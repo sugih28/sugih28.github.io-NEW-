9a8f88153a5a3f41e869e34b1e1e8d8e21370944
Sugih Ilmi Kalih Putra <sugih.ikp@gmail.com>
2019-06-17 13:32:31 +0700

    Updates

---
+++ precache-manifest.5beb0fe1162787c677c3743e3e041fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38868fac685b74bf4d9137fc1de79f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a417fca57ec3403242",
+    "url": "/static/css/main.10106fd0.chunk.css"
+  },
+  {
+    "revision": "25f165e232d386198ef6",
+    "url": "/static/js/2.65778322.chunk.js"
+  },
+  {
+    "revision": "b3a417fca57ec3403242",
+    "url": "/static/js/main.2a48bd0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "418c52ca021cbd03f606afe40db1efc7",
+    "url": "/static/media/banner1.418c52ca.jpg"
+  }
+]);
